57bb24df9d9c40c1a68babef2e6ca4ded55cd4e0
Dofe62335 <dofe62335@eleve.isep.fr>
2025-06-17 14:18:39 +0200

    Update login.php

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -1,36 +1,31 @@
 <?php
-require_once __DIR__ . '/../../controllers/AuthController.php';
+require_once __DIR__ . '/../config/db.php';
 
-$message = '';
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $message = AuthController::login($_POST['email'], $_POST['password']);
+class Capteur {
+    public static function getLastTemperatureData($limit = 10) {
+        global $pdo;
+        $stmt = $pdo->prepare("SELECT temp, date_time_temp FROM test_temperature ORDER BY date_time_temp DESC LIMIT ?");
+        $stmt->execute([$limit]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function getLastButaneData($limit = 10) { // ← nouveau
+        global $pdo;
+        $stmt = $pdo->prepare("SELECT value, timestamp, alert FROM capt_butane ORDER BY timestamp DESC LIMIT ?");
+        $stmt->execute([$limit]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function getLastPresenceData($limit = 1) {
+        global $pdo;
+        $stmt = $pdo->prepare("SELECT personnes_count, distance_cm, timestamp FROM proximity_sensor ORDER BY timestamp DESC LIMIT ?");
+        $stmt->execute([$limit]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    public static function getActiveBuzzers() {
+    global $pdo;
+    $stmt = $pdo->query("SELECT * FROM buzzer WHERE flagactivation = true ORDER BY id_alerte ASC");
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
-?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Connexion</title>
-    <link rel="stylesheet" href="/volpe-site/public/assets/css/auth.css">
-</head>
-<body>
-    <div class="login-container">
-        <h2>Connexion</h2>
-
-        <form method="POST">
-            <input type="email" name="email" placeholder="Email" required>
-            <input type="password" name="password" placeholder="Mot de passe" required>
-            <button type="submit">Se connecter</button>
-        </form>
-
-        <?php if ($message): ?>
-            <p style="color: red;"><?= htmlspecialchars($message) ?></p>
-        <?php endif; ?>
-
-        <div class="back-home">
-            <a href="/volpe-site/public/index.php">← Retour à la page d'accueil</a>
-        </div>
-    </div>
-</body>
-</html>
+}
